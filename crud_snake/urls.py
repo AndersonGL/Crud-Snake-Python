3f49e779fe1d9ba3da3b387aa93de50828dee12a
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-03-17 21:59:31 -0300

    atulizacao crud

--- crud_snake/urls.py
+++ crud_snake/urls.py
@@ -1,14 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path
-from meuapp.views import home, form, create, view 
+from meuapp.views import home, form, create, view, edit, update, delete  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('form/', form, name='form'),
     path('create/', create, name='create'),  # Criando a rota create
-    path('view/<int:pk/', view, name='view'),
+    path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update'),
+    path('delete/<int:pk>/', delete, name='delete'),
 ]
 
 
--- meuapp/views.py
+++ meuapp/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from meuapp.forms import CarroForm
 from meuapp.models import Carro
 
 
 
-def home(request):
+def home(request):                      
     data = {}
     data['db'] = Carro.objects.all()
     return render(request, 'index.html', data)
    
 
-
 def form(request):
     data = {}
     data['form'] = CarroForm()          # mandando o form para o form.html
     return render(request, 'form.html', data )
-
-
 
 
 def create(request):
@@ -31,4 +27,24 @@ def view (request, pk):
     data = {}
     data['db'] = Carro.objects.get(pk=pk)
     return render(request, 'view.html', data)
+
+
+def edit (request, pk):
+    data = {}
+    data['db'] = Carro.objects.get(pk=pk)
+    data['form'] = CarroForm(instance=data['db'])
+    return render(request, 'form.html', data)
     
+
+def update(request, pk):
+    data = {}
+    data['db']= Carro.objects.get(pk=pk)
+    form = CarroForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+    return redirect('home')
+    
+def delete(request, pk):
+    db = Carro.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
